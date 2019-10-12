ec6468c9d8cbec7e2ae11f60997b3272a723886c
jaewonYoon <21300492@handong.edu>
2019-10-12 11:32:07 +0900

    mastery done

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 const VideoDetail = ({video}) => {
+    if(video ===null){
+        return "search and select video";
+    }
+    else{
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`; 
+    
     return video===null? "search and select video" :(
         <div>
-            {video.snippet.title}
+            <div className="ui embed">
+                <iframe src={videoSrc} title={video.snippet.title}/>
+            </div>
+            <div className="ui segment">
+                <h4 className="ui header">{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
+            </div>
         </div>
         )
+    }
 };
 
 export default VideoDetail;
