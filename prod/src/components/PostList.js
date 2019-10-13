d7f63ccaf2c99be50d3fd46083129125d130c976
jaewonYoon <21300492@handong.edu>
2019-10-13 09:54:19 +0900

    integrating react and redux

---
+++ prod/src/components/PostList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostList from './PostList';
+class PostList extends React.Component{
+    render(){
+        return(
+            <div className="ui container">
+                <PostList/>
+            </div>
+
+        ) 
+    };
+};
